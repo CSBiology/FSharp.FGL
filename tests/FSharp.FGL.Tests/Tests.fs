f0777da7bfe6cd7dc4c0a49e5e684ee2d4419f27
Timo Mühlhaus <Timo Mhlhaus>
2017-03-28 15:13:28 +0200

    Initial commit

---
+++ tests/FSharp.FGL.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module FSharp.FGL.Tests
+
+open FSharp.FGL
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
