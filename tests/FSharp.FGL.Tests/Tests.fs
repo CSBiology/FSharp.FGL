7a5222b831c27dfff4596fcfc2b6eb4850502dd3
WalternativE <gregor.beyerle@gmail.com>
2018-03-10 00:22:59 +0100

    cleaned up a bit *changed build tooling to dotnet cli *adapted build file *added new sourcelink dependencies and validation cli *wired up xunit testing framework

--- tests/FSharp.FGL.Tests/Tests.fs
+++ tests/FSharp.FGL.Tests/Tests.fs
@@ -1,10 +1,8 @@
 module FSharp.FGL.Tests
-//
-//open FSharp.FGL
-//open NUnit.Framework
-//
-//[<Test>]
-//let ``hello returns 42`` () =
-//  let result = Library.hello 42
-//  printfn "%i" result
-//  Assert.AreEqual(42,result)
+
+open FSharp.FGL
+open Xunit
+
+[<Fact>]
+let ``true is always true`` () =
+    Assert.True(true)
