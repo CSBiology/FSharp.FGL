0fee5bbc4634a86f31c7471745d2dd092a414790
Kevin Schneider <k3v1n_schneider@gmx.de>
2018-08-09 14:10:06 +0200

    Migration to new ProjectScaffold

--- src/FSharp.FGL/AssemblyInfo.fs
+++ src/FSharp.FGL/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.FGL")>]
 [<assembly: AssemblyProductAttribute("FSharp.FGL")>]
 [<assembly: AssemblyDescriptionAttribute("Functional graph library for F#")>]
-[<assembly: AssemblyVersionAttribute("0.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.6")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.FGL"
     let [<Literal>] AssemblyProduct = "FSharp.FGL"
     let [<Literal>] AssemblyDescription = "Functional graph library for F#"
-    let [<Literal>] AssemblyVersion = "0.6"
-    let [<Literal>] AssemblyFileVersion = "0.6"
+    let [<Literal>] AssemblyVersion = "0.0.1"
+    let [<Literal>] AssemblyFileVersion = "0.0.1"
     let [<Literal>] AssemblyConfiguration = "Release"
---
+++ tests/FSharp.FGL.Tests/RunTests.fs
@@ -0,0 +1,13 @@
+namespace FSharp.FGL.Tests
+
+open Expecto
+
+module RunTests =
+
+    [<EntryPoint>]
+    let main args =
+
+        Tests.runTestsWithArgs defaultConfig args Tests.testSimpleTests |> ignore
+
+        0
+
