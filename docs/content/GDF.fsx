24f4f0b9e42e70aa2bf55f0f0220966c901e88f4
Libra_Chris <59606965+LibraChris@users.noreply.github.com>
2021-06-13 16:51:50 +0200

    update GDF dokumentation

--- docs/content/GDF.fsx
+++ docs/content/GDF.fsx
@@ -1,12 +1,11 @@
 (*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
-#I @"../../bin/FSharp.FGL/net47/"
-#I @"C:\Users\Christopher\source\repos\LibraChris\FSharp.FGL\bin\FSharp.FGL\net47"
-#I @"C:\Users\Christopher\source\repos\LibraChris\FSharp.FGL\bin\FSharp.FGL.IO\net47"
-#r "Aether.dll"
+#I @"../../bin\FSharp.FGL/netstandard2.0/"
+#I @"../../bin\FSharp.Graph/netstandard2.0/"
+
 #r "FSharp.FGL.dll"
-#r "FSharp.FGL.IO.dll"
+#r "FSharp.Graph.dll"
 (**
 # GDF Format
 GDF is the file formate used by [The Graph Exploration System (GUESS)](http://graphexploration.cond.org/) . It is build similar to a database table or <abbr title="Comma Seperated File">CSV</abbr>. Both edge and node data is defined in a single file, each initiated by their respective headers. Each element (i.e. node or edge) is on a line and values are separated by comma. Node definition is started by "nodedef>name" and edge definition by "edgedef>node1". For nodes, only the name is needed to build, but for edges both “node1” and “node2” are required, which are the names of the two nodes you are connecting.  
@@ -39,9 +38,8 @@ This example demonstrates a possible gdf data structure.
 ## Reading GDF files
 To read GDF files, just use the ofFile function located in the gdf module of the FSharp.FGL.IO namespace. It does not need anything but the file path ans will return the vertices and edges as a vertex list, edge list tupel.
 *)
-open Aether
 
-open FSharp.FGL.IO
+open FSharp.Graph.IO
 
 let fileDir = __SOURCE_DIRECTORY__ + "/data/"
 
@@ -56,24 +54,22 @@ let gdfFileRead = GDF.fromFile path
 Additionally, you can use the fromArray function instead of fromFile to directly transform an array to a vertex list, edge list tupel.
 <br>
 *)
-open Aether
 
-open FSharp.FGL.IO
+open FSharp.Graph.IO
 
 let gdfArrayRead = GDF.fromArray [|"nodedef>name VARCHAR,label VARCHAR";"s1,Site number 1";"s2,Site number 2";"edgedef>node1 VARCHAR,node2 VARCHAR,weight DOUBLE";"s1,s2,1.2341"|]
 
 (**
 ##Writing GDF format files
-To save a graph that takes the same form as a graph created by the reading GDF file functions, the toFile function can be applied. Additionally to the graph and the filepath, the state of the graph(directed/undirected) has to be stated. In case of an undirected graph, the addendum false is used.
+To save a graph that takes the same form as a graph created by the reading GDF file functions, the toFile function can be applied. This function needs the vertex list and the edge list of the graph and saves the gdf file at the given path.
 *)
-open Aether
 
-open FSharp.FGL.IO
+open FSharp.Graph.IO
 
-open FSharp.FGL.Directed
+let pathSave    = fileDir + "GDFExampleToFile.txt"
 
-let pathSave = fileDir + "GDFExampleToFile.txt"
+let vertexList  = (fst gdfFileRead)
 
-let graph = Graph.create (fst gdfFileRead) (snd gdfFileRead)
+let edgeList    = (snd gdfFileRead)
 
-GDF.toFile graph true pathSave
+GDF.toFile vertexList edgeList pathSave
