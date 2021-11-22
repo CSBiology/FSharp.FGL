72f3e6c1a4f9898af20009c721df29ec2efd8aa5
Christopher Lux <59606965+LibraChris@users.noreply.github.com>
2021-11-22 14:37:38 +0100

    include the new tests in runTests

--- tests/FSharp.FGL.Tests/Main.fs
+++ tests/FSharp.FGL.Tests/Main.fs
@@ -8,8 +8,14 @@ let main argv =
     //FSharp.FGL core tests
     Tests.runTestsWithCLIArgs [] argv IOTests.testGDFReaderFuntions                 |>ignore
     Tests.runTestsWithCLIArgs [] argv IOTests.testGDFWriterFuntions                 |>ignore
+
     Tests.runTestsWithCLIArgs [] argv ArrayAdjacencyGraphTests.testInitializeGraph  |>ignore
     Tests.runTestsWithCLIArgs [] argv ArrayAdjacencyGraphTests.testEdges            |>ignore
     Tests.runTestsWithCLIArgs [] argv ArrayAdjacencyGraphTests.testVertices         |>ignore
     Tests.runTestsWithCLIArgs [] argv ArrayAdjacencyGraphTests.testLabels           |>ignore
+
+    Tests.runTestsWithCLIArgs [] argv FGLTests.testInitializeGraph                  |>ignore
+    Tests.runTestsWithCLIArgs [] argv FGLTests.testFGLGraph                         |>ignore
+
+
     0
